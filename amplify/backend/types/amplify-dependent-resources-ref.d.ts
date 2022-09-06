f01616d13b39eae835552236f1c4b2e534549ef4
seankeish111 <shankesh.s@outlook.com>
2022-09-06 23:52:08 +1000

    amp init complted with data-models

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "removalist": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
